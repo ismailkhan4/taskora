26f5c01e6dddedd31e73c5dd1b12cb77394abdba
ismailkhan4 <islamians96@gmail.com>
2026-09-15 11:45:36 +0500

    chore(ui): Implemented Sidebar for both Desktop and Mobile View

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 
 export default async function Home() {
   const user = await getCurrent();
@@ -9,7 +8,7 @@ export default async function Home() {
   if (!user) redirect('/sign-in')
   return (
     <div>
-      <UserButton />
+      This is a home page
     </div>
   );
 }
